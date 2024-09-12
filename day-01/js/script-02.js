3c970d268c08ad32aba53d7bbdb5b29b344dbddd
kubraaydogdu <huseyincan.gunduz@solution-factory.eu>
2024-09-12 16:32:10 +0300

    12Eylül/2024

--- day-01/js/script-02.js
+++ day-01/js/script-02.js
@@ -1,3 +1,10 @@
 console.log("Hello Worl");
 
 let finalNotu = 12; //camelCase 
+
+/*
+JavaScript kodunu yazmak için bir .js dosyası oluşturabilirsiniz, 
+fakat bu dosyanın tarayıcıda çalışabilmesi için HTML dosyasında çağrılması gerekir. 
+JavaScript, tarayıcıda çalışırken, genellikle bir HTML belgesine gömülü olarak kullanılır 
+veya harici bir .js dosyası olarak HTML dosyasına eklenir.
+*/
